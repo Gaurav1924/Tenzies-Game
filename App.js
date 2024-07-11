fb16210effd486c565baf7859cad75699b5eebb1
unknown <gauravkshatriya99@gmail.com>
2024-07-11 12:36:00 +0530

    bug fix

--- App.js
+++ App.js
@@ -48,9 +48,9 @@ export default function App() {
     
     function holdDice(id) {
         setDice(oldDice => oldDice.map(die => {
-            return die.id === id ? 
+            return (die.id === id ? 
                 {...die, isHeld: !die.isHeld} :
-                die
+                die)
         }))
     }
     
